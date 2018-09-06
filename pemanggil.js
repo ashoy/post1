10324657c5eff59a927b0b0a5f5244f847f4c729
ashoy <42585316+ashoy@users.noreply.github.com>
2018-09-06 12:38:14 -0400

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -18,14 +18,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
-var xxx=null;
-if(x=="1"){xxx="/2018/08/how-to-get-personal-injury-protection.html"}
-if(x=="2"){xxx="/2018/08/quick-car-insurance-quote-online.html"}
-if(x=="3"){xxx="/2018/08/how-to-advantages-of-small-car.html"}
-if(x=="4"){xxx="/2018/08/top-best-car-insurance-companies.html"}
-if(x=="5"){xxx="/2018/08/cheap-car-insurance-quotes.html"}a_to_ve[i].href="https://autoinsurancelinx.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="http://autoinsurancelinx.blogspot.com/2018/08/how-to-get-personal-injury-protection.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
